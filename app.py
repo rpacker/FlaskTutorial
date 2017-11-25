eccc03a9ef0b0db4b2e4776254709c47bc3e728a
robert <robert@c4s.com>
2017-11-25 18:20:53 -0500

    Updated to app.py with put and delete.

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
 
 app = Flask(__name__)
+app.secret_key = 'rob'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {"item": "None"}, 200 if item else 404
 
-    def somejunk():
-        pass
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-api.add_resource(Student, '/student/<string:name>')  # http://127.0.0.1:5000/stucent/Rolf
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)  # default is 5000
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+    def delete(self, name):
+        global items
+        item = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)  # default is 5000

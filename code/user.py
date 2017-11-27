3150445533be08e1cb8d2493b2eacdfb37c473fd
Rep <repacker@gmail.com>
2017-11-26 21:10:22 -0500

    ...

--- code/user.py
+++ code/user.py
@@ -1,37 +1,32 @@
 import sqlite3
+from flask_restful import Resource, reqreqparse
 
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
+class User:=
 
-    @classmethod
-    def find_by_username(cls, username):
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str,
+        required=true,
+        help="This field cannot be blank"
+    )
+
+    parser.add_argument('password',
+        type=str,
+        required=true,
+        help="This field cannot be blank"
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse.
+        
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE username = ?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(row[0], row[1], row[2])
-        else:
-            user = None
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username', data['password']]))
 
+        connection.commit()
         connection.close()
-        return user
 
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM users WHERE id = ?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(row[0], row[1], row[2])
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return {'message': 'User created successfully'}, 201

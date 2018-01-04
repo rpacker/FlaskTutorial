96debcb77c6357cd3025bfe5cdbad9c9735b618d
Robert <repacker@gmail.com>
2018-01-03 21:49:31 -0500

    Ch 6.

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-from model.item import ItemModel
+from models.item import ItemModel
 
 
 class Item(Resource):

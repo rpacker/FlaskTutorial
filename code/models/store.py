f3cdbd21f6287a488785250dd7f689a9b97101ae
robert <robert@c4s.com>
2018-01-08 20:45:43 -0500

    ch 6 update

--- code/models/store.py
+++ code/models/store.py
@@ -2,17 +2,15 @@ import sqlite3
 from db import db
 
 class StoreModel(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
     items = db.relationship('ItemModel', lazy='dynamic')
 
-    def __init__(self, name, price, store_id):
+    def __init__(self, name):
         self.name = name
-        self.price = price
-        self.store_is = store_id
 
     def json(self):
         return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
